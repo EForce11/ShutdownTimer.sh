ac74862abbc7819e5738d91b7c1f3c3d4e8c97fd
EForce11 <94642610+EForce11@users.noreply.github.com>
2024-05-17 01:34:35 +0300

    Update shutdownTimer.sh

--- shutdownTimer.sh
+++ shutdownTimer.sh
@@ -8,18 +8,24 @@ while true; do
     read -p "Please select a process (1 or 2): " selection
 
     if [ "$selection" == "1" ]; then
-        read -p "Enter the shutdown time in minutes:seconds (e.g., 2:30): " minute_second
+        read -p "Enter the shutdown time in minutes:seconds (e.g., 40:20): " minute_second
 
         IFS=':' read -r minute second <<< "$minute_second"
-        total_second=$((minute*60+second))
+        total_seconds=$((minute * 60 + second))
+
+        current_time=$(date +%s)
+        shutdown_time=$((current_time + total_seconds))
+        shutdown_time_formatted=$(date -d @$shutdown_time +%H:%M:%S)
 
         echo
-        shutdown -h $total_second
+        echo "The system will shut down at $shutdown_time_formatted."
+        shutdown -h +$((total_seconds / 60))
         break
 
     elif [ "$selection" == "2" ]; then
         echo
         shutdown -c
+        echo "Shutdown has been canceled."
         break
 
     else
